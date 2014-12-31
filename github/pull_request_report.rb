945458203b5eda2a322f364f415bb623af34d02a
Robert Fischer <robert.fischer@weboniselab.com>
2014-12-31 14:15:21 -0500

    Multiple repos

--- github/pull_request_report.rb
+++ github/pull_request_report.rb
@@ -33,12 +33,14 @@ puts "Executing as #{Octokit.user.login}"
 
 # Retrieve the pull requests
 pulls = []
-BRANCHES.each do |branch|
-  puts "Retrieving #{PULL_STATE} pull requests against #{branch} for #{REPO}"
-  pulls = pulls + Octokit.pulls(REPO, {
-    :state => PULL_STATE,
-    :base => branch
-  })
+REPOS.each do |repo|
+  BRANCHES.each do |branch|
+    puts "Retrieving #{PULL_STATE} pull requests against #{branch} for #{repo}"
+    pulls = pulls + Octokit.pulls(repo, {
+      :state => PULL_STATE,
+      :base => branch
+    })
+  end
 end
 
 # Print out a pull request to ensure you see them
@@ -50,7 +52,18 @@ end
 # Count pulls by user
 puts "Pull Requests by User:"
 pulls_by_user = Hash.new { |hash, key| hash[key] = 0 }
+last_pull_by_user = Hash.new { |hash,key| Time.new(0) }
 pulls.each do |pull|
-  pulls_by_user[pull.user.login] += 1
+  user = pull.user.login
+
+  pulls_by_user[user] += 1
+
+  prev_date = last_pull_by_user[user]
+  curr_date = pull.created_at
+  last_pull_by_user[user] = curr_date if prev_date < curr_date
 end
-print_hash(pulls_by_user)
+joined_hash = Hash.new
+pulls_by_user.keys.each do |user|
+  joined_hash[user] = pulls_by_user[user].to_s + "\t" + last_pull_by_user[user].to_s
+end
+print_hash joined_hash
