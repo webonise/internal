959be41847c65642c8e8e1b8367721b20179da43
Robert Fischer <robert.fischer@weboniselab.com>
2015-01-26 11:42:56 -0500

    Update pull_request_report to have the init script

--- github/config.sample.rb
+++ github/config.sample.rb
@@ -1,8 +1,7 @@
-USERNAME = "RobertFischer"
-PASSWORD = "My Awesome Password"
-REPOS = ["Farm-Server/FarmServer"]
+USERNAME = # Put your GitHub username here
+PASSWORD = # Put your GitHub password here
 BRANCHES = ['stable','develop']
-PULL_STATE = "all" # open, closed, or all
 API_ENDPOINT = 'http://github.beckshybrids.com/api/v3/'  # Leave blank for GitHub proper
 WEB_ENDPOINT = 'http://github.beckshybrids.com'          # Leave blank for GitHub proper
 DEBUG = false
+
--- github/pull_request_report.rb
+++ github/pull_request_report.rb
@@ -1,41 +1,12 @@
-require 'octokit'
-require 'faraday/http_cache'
-require 'pp'
-require './config.rb'
+require_relative 'init'
 
-# Describes out to print a hash: if you don't like it, define it in your config.rb
-unless self.respond_to? :print_hash
-  def print_hash(hash)
-    hash.each do |k,v|
-      printf "%20s => %s\n", k, v
-    end
-  end
-end
-
-# Provide authentication credentials
-Octokit.configure do |c|
-  c.login = USERNAME
-  c.password = PASSWORD
-  c.auto_paginate = true
-  c.api_endpoint = API_ENDPOINT unless API_ENDPOINT.empty? if API_ENDPOINT
-  c.web_endpoint = WEB_ENDPOINT unless WEB_ENDPOINT.empty? if WEB_ENDPOINT
-end
-
-# Cache responses to stretch our rate limit
-Octokit.middleware = Faraday::RackBuilder.new do |builder|
-  builder.use Faraday::HttpCache
-  builder.use Octokit::Response::RaiseError
-  builder.adapter Faraday.default_adapter
-end
-
-# Basic sanity check
-puts "Executing as #{Octokit.user.login}"
+PULL_STATE = "all" unless defined? PULL_STATE
 
 # Retrieve the pull requests
 pulls = []
 REPOS.each do |repo|
   BRANCHES.each do |branch|
-    puts "Retrieving #{PULL_STATE} pull requests against #{branch} for #{repo}"
+    LOGGER.info "Retrieving #{PULL_STATE} pull requests against #{branch} for #{repo}"
     pulls = pulls + Octokit.pulls(repo, {
       :state => PULL_STATE,
       :base => branch
@@ -45,7 +16,7 @@ end
 
 # Print out a pull request to ensure you see them
 if DEBUG
-  puts "Sample pull request:"
+  LOGGER.info "Sample pull request:"
   PP.pp(pulls[0])
 end
 
