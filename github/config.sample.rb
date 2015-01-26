4a2fc94dc21b8a142393cdbb62bf063bf3b0997d
Robert Fischer <robert.fischer@weboniselab.com>
2015-01-26 17:06:15 -0500

    Remove BRANCHES from the common config

--- github/config.sample.rb
+++ github/config.sample.rb
@@ -1,6 +1,5 @@
 USERNAME = # Put your GitHub username here
 PASSWORD = # Put your GitHub password here
-BRANCHES = ['stable','develop']
 API_ENDPOINT = 'http://github.beckshybrids.com/api/v3/'  # Leave blank for GitHub proper
 WEB_ENDPOINT = 'http://github.beckshybrids.com'          # Leave blank for GitHub proper
 DEBUG = false
