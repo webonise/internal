a54fa99046fb381e6cbc50e8ed583dc532cff853
Robert Fischer <robert.fischer@weboniselab.com>
2015-01-26 17:08:05 -0500

    Needs to be specific about the repos and the branches now that those aren't in the common config

--- github/pull_request_report_config.sample.rb
+++ github/pull_request_report_config.sample.rb
@@ -1,1 +1,3 @@
+REPOS = ["Farm-Server/FarmServer", "Farm-Server/cdc"] # Which repos do you want to look for PRs on?
+BRANCHES = ['stable','develop']  # Pull requests to merge into which branch?
 PULL_STATE = "all" # open, closed, or all -- which pull requests count?
